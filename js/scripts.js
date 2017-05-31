81d75021ad8a8cadcd91f6857454a85b2d4ecd84
Laura Hamilton and Michael Dunlap <michael@>
2017-05-31 12:02:43 -0700

    fix functionality of hi button.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
   });
+
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
@@ -13,4 +20,6 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
   });
+
+
 });
